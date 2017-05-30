03dd1fb6b31b36049310cc7cf6942354accd8f81
Jonathan Phillipo <jdphil@gmail.com>
2017-05-30 21:58:21 +0100

    getting data from lloyds

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const LLOYDS = 'https://api.lloydsbank.com/open-banking/v1.2/personal-current-accounts';
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      result: null
+    };
+
+    this.setBankData = this.setBankData.bind(this);
+    this.fetchBankData = this.fetchBankData.bind(this);
+}
+    setBankData(result) {
+      this.setState({ result });
+    }
+
+    fetchBankData(){
+      fetch(LLOYDS)
+      .then(response =>
+        response.json())
+      .then(result => this.setBankData(result));
+    }
+
+    componentDidMount() {
+      this.fetchBankData();
+    }
+
   render() {
+    const { result } = this.state;
+
+    if (!result) { return null ;}
+
+    let list = result.data;
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        { list.map(item =>
+          <div key={item.ProductName}>{item.ProductName}</div>
+        )}
       </div>
     );
   }
