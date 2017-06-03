5c625221a4e3d994499fb23ba2f77b8b55f54ac8
Jonathan Phillipo <jdphil@gmail.com>
2017-06-03 23:22:50 +0100

    brutal async

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 const LLOYDS = 'https://api.lloydsbank.com/open-banking/v1.2/personal-current-accounts';
+const HALIFAX = 'https://api.halifax.co.uk/open-banking/v1.2/personal-current-accounts';
 
 class App extends Component {
 
@@ -16,14 +17,35 @@ class App extends Component {
     this.fetchBankData = this.fetchBankData.bind(this);
 }
     setBankData(result) {
+      //console.log(result);
       this.setState({ result });
     }
 
     fetchBankData(){
-      fetch(LLOYDS)
+      let apiReq1 = fetch(HALIFAX)
       .then(response =>
-        response.json())
-      .then(result => this.setBankData(result));
+        response.json());
+      //.then(result => this.setBankData(result));
+
+      let apiReq2 = fetch(LLOYDS)
+      .then(response =>
+        response.json());
+
+      let combinedData = {"api1":{},"api2":{}};
+
+      Promise.all([apiReq1,apiReq2])
+      .then(function(values){
+       combinedData["api1"] = values[0];
+       //console.log(values[0]);
+       //console.log(values[1]);
+       combinedData["api2"] = values[1];
+       //console.log(combinedData);
+        })
+      .then(this.setBankData(combinedData));
+
+
+
+      //.then(result => this.setBankData(result));
     }
 
     componentDidMount() {
@@ -35,12 +57,20 @@ class App extends Component {
 
     if (!result) { return null ;}
 
-    let list = result.data;
+    const util = require('util')
+
+    console.log(util.inspect(this.state, {showHidden: false, depth: null}));
+
+    console.log(this.state);
+
+
+    let list = result;
+    //console.log(list);
 
     return (
       <div className="App">
         { list.map(item =>
-          <div key={item.ProductName}>{item.ProductName}</div>
+          <div key={item.ProductIdentifier}><p>{item.ProductName}</p></div>
         )}
       </div>
     );
